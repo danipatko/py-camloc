cec4ecbee7938e184dfb043f664fc0994590a1cd
danipatko <p8.pada@gmail.com>
2023-02-16 22:26:20 +0100

    update save

--- custom_save.py
+++ custom_save.py
@@ -6,14 +6,15 @@ from util import load_remap
 def save_custom(name: str, map1, map2):
     with open(name + "1.sex", "wb") as f:
         shape = np.shape(map1)
-        f.write(struct.pack(f"{len(shape)}i", *shape))
+        f.write(struct.pack("3i", *shape))
         for row in map1:
             for col in row:
-                f.write(struct.pack(f"{shape[2]}i", *col))
+                f.write(struct.pack(f"2h", *col))
 
+    # NOTE: the shape's bytes must be fixed
     with open(name + "2.sex", "wb") as f:
         shape = np.shape(map2)
-        f.write(struct.pack(f"{len(shape)}i", *shape))
+        f.write(struct.pack("2i", *shape))
         for row in map2:
             for col in row:
                 f.write(struct.pack("H", col))
